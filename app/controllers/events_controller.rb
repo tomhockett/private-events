13d12fd54c9ad5e3acdd0863c8cb8b69891c0062
Tom Hockett <tom.hockett@gmail.com>
2021-07-24 14:50:16 -0500

    Add form partial and create actions for new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,32 @@
 ##
 # Events Controller
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @events = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
   def show
     @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Your event was saved! 🎉'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title)
   end
 end
